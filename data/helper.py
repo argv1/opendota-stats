66b952f5c7fc55de8868629f9c9de19249bba2ea
argv1 <github@hofa.biz>
2022-11-27 13:37:39 +0100

    Update helper.py

--- data/helper.py
+++ data/helper.py
@@ -33,7 +33,8 @@ def main():
 
     # write new dataframe to csv
     df_full.to_csv("hero_lore.csv", index=False)
-    # keep in mind to use a converter rereading the csv i.e. heroes = pd.read_csv("hero_lore.csv", index_col="id" ,converters={"roles": lambda x: x.strip("[]").replace("'","").split(", ")})
+    # keep in mind to use a converter rereading the csv since the column roles will be interpreted as a string and not as a list
+    # i.e. heroes = pd.read_csv("hero_lore.csv", index_col="id" ,converters={"roles": lambda x: x.strip("[]").replace("'","").split(", ")})
     # or consider using pd.to_pickle and pd.read_pickle or pd.to_parquet and pd.read_parquet    
 
 if __name__ == '__main__':
